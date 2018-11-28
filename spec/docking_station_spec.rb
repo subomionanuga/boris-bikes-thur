9faaac1aa0ab1f20051bebfd0defbce68f9f7198
Carol Gardiner <carolan.gardiner@gmail.com>
2018-11-28 15:44:39 +0000

    Testing for release case

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
+  it 'gets a bike and check if working' do
+    bike = DockingStation.new.release_bike
+    expect(bike).to be_working
+  end
+
 end

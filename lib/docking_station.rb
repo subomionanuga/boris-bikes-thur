5ff84d8eee5ea947eb2c0aa89d38b7d1177589c5
Adam Lodowski <adamlodowski@gmail.com>
2018-11-29 15:38:42 +0000

    DockingStation no longer release bike if none available

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,7 +5,11 @@ class DockingStation
   attr_reader :bike
     
   def release_bike
-    Bike.new
+    if @bike.nil?
+      raise "The bike does not exist"
+    else
+      @bike
+    end
   end
 
   def return_bike(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,16 @@
-require 'docking_station.rb'
+require "docking_station.rb"
 
 describe DockingStation do
-  it 'responds to release_bike' do
+  it "responds to release_bike" do
     expect(subject).to respond_to :release_bike
   end
-  it 'gets a bike and check if working' do
-    bike = subject.release_bike
+
+  it "gets a bike and checks if working" do
+    bike = Bike.new
     expect(bike.working?).to eq true
   end
   
-  it 'Checks if DockingStation can respond to bike with argument' do
+  it "Checks if DockingStation can respond to bike with argument" do
     expect(subject).to respond_to(:return_bike).with(1).argument  
   end
   
@@ -17,7 +18,7 @@ describe DockingStation do
     expect(subject).to respond_to(:bike)
   end
 
-  it 'lets user return bike to docking station' do
+  it "lets user return bike to docking station" do
     bike = Bike.new
     expect(subject.return_bike(bike)).to eq bike
   end
@@ -30,6 +31,10 @@ describe DockingStation do
 
   it "checks if docking station is empty" do
     expect(subject.bike).to eq nil
+  end
+
+  it "raises an error if docking station has no bike" do
+    expect{ subject.release_bike }.to raise_error("The bike does not exist")
   end
 
 end
